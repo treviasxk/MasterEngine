9e08daf29bcbc16c28addbd535106ead32879646
Trevias Xk <trevias@live.com>
2024-10-27 22:50:04 -0300

    Changed OpenGL to Linux.

--- MasterEngine.Editor/editor/windows/EditorWindow.axaml.cs
+++ MasterEngine.Editor/editor/windows/EditorWindow.axaml.cs
@@ -68,9 +68,9 @@ public partial class EditorWindow : Window{
         gridDock.Add(tabControl4);
 
 
-        managerGraphic = new ManagerGraphic(Scene.Control, GraphicAPI.Direct3D11);
+        //managerGraphic = new ManagerGraphic(Scene.Control, GraphicAPI.OpenGL);
         Title = Application.EngineName + " - " + managerGraphic?.API;
-        managerGraphic!.GraphicComponent!.OnUpdate += OnUpdate;
+        //managerGraphic!.GraphicComponent!.OnUpdate += OnUpdate;
     }
 
     private void OnUpdate(double obj){
